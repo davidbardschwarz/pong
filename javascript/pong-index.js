57b9972462a4917327864e2dddc3b355b3760f41
davidbardschwarz <63382529+davidbardschwarz@users.noreply.github.com>
2023-01-11 14:54:51 -0600

    Update pong-index.js

--- javascript/pong-index.js
+++ javascript/pong-index.js
@@ -16,9 +16,9 @@ import {
   adventureMusic,
   villageMusic,
   playDeferredSounds
-} from "./pong-audio.js";
+} from "./javascript/pong-audio.js";
 //Defaults for game objects
-import { Game, Ball, Paddle } from "./pong-classes.js";
+import { Game, Ball, Paddle } from "./javascript/pong-classes.js";
 //Game Events
 import {
   GameEvent,
@@ -34,9 +34,9 @@ import {
   muteControl,
   volumeUpControl,
   volumeDownControl
-} from "./pong-events.js";
+} from "./javascript/pong-events.js";
 //Utility functions
-import { clamp, scalerange, randomAdjust, boolToOnOff } from "./pong-util.js";
+import { clamp, scalerange, randomAdjust, boolToOnOff } from "./javascript/pong-util.js";
 //import * as Tone from "../lib/Tone.js";
 
 //GAME OBJECTS///DON'T CHANGE THESE
