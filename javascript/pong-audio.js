f90f619d27b12764b6a218083e2597293dd1cd08
davidbardschwarz <63382529+davidbardschwarz@users.noreply.github.com>
2023-12-20 11:27:23 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -52,7 +52,7 @@ soundArray.push(paddleSound);
 export var scoreSound = new soundFile("silence.mp3");
 soundArray.push(scoreSound);
 
-export var ambientSound = new soundFile("ambient.mp3");
+export var ambientSound = new soundFile("beach.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
 ambientSound.player.volume.value = -20; //turn down volume
